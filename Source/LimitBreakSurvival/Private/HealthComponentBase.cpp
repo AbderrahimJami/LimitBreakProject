db2f30c0a6f5a137d7fbfb1cf7064978dcbf5386
Abderrahim Jami <abdrrahimjami@outlook.com>
2024-09-25 22:46:05 +0100

    Create new function and variables inside HealthComponentBase

--- Source/LimitBreakSurvival/Private/HealthComponentBase.cpp
+++ Source/LimitBreakSurvival/Private/HealthComponentBase.cpp
@@ -9,8 +9,58 @@ UHealthComponentBase::UHealthComponentBase()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
+	
+	
+
+	
 
 	// ...
+}
+
+void UHealthComponentBase::Heal(float healAmount)
+{
+	if (CurrentHealth == healAmount) return;
+	CurrentHealth += healAmount;
+	if (CurrentHealth >= MaxHealth)
+		CurrentHealth = MaxHealth;
+	//Broadcast Health Change
+	OnHealthChanged.Broadcast(CurrentHealth);
+
+}
+
+bool UHealthComponentBase::IsDead() const
+{
+	return isDead;
+}
+
+void UHealthComponentBase::TakeDamage(float damageAmount)
+{
+	
+	CurrentHealth -= damageAmount;
+	//Check if there's an active timer 
+	
+	//Letting code know health changed
+	OnHealthChanged.Broadcast(CurrentHealth);
+	if (CurrentHealth <= 0)
+	{
+		isDead = true;
+		//Letting rest of the code know the Owning Actor's dead now 
+		OnDeath.Broadcast();
+	}
+	
+}
+
+void UHealthComponentBase::ApplyHealthOverTime(float DamagePerSecond, float Duration)
+{
+	timerDelegate.BindUFunction(this, FName("Heal"), DamagePerSecond);
+	FTimerHandle durationTimer;
+	GetWorld()->GetTimerManager().SetTimer(Timer, timerDelegate, 1.0f, true);
+	GetWorld()->GetTimerManager().SetTimer(durationTimer, FTimerDelegate::CreateLambda([&]
+	{
+		GetWorld()->GetTimerManager().ClearTimer(Timer);
+
+	}), Duration, false);
+	
 }
 
 
--- Source/LimitBreakSurvival/Public/HealthComponentBase.h
+++ Source/LimitBreakSurvival/Public/HealthComponentBase.h
@@ -6,8 +6,9 @@
 #include "Components/ActorComponent.h"
 #include "HealthComponentBase.generated.h"
 
-
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnHealthChanged, float, newHealth);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnDeath);
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent), Blueprintable )
 class LIMITBREAKSURVIVAL_API UHealthComponentBase : public UActorComponent
 {
 	GENERATED_BODY()
@@ -16,6 +17,30 @@ public:
 	// Sets default values for this component's properties
 	UHealthComponentBase();
 
+	/*
+	Increases current health, up to the maximum health limit 
+	 */
+	UFUNCTION(BlueprintCallable, Category="Health")
+	void Heal(float healAmount = 10.f);
+	
+	
+	UFUNCTION(Blueprintable, Category = "Health")
+	bool IsDead() const;
+
+	/*
+	Reduces current health based on the damage received, potentially
+	triggering death if health reaches zero.
+	 */
+	UFUNCTION(BlueprintCallable, Category="Damage Handling")
+	void TakeDamage(float damageAmount = 10);
+
+	/*
+	Applies gradual damage over a set duration
+	 */
+	UFUNCTION(BlueprintCallable, Category="Health")
+	void ApplyHealthOverTime(float DamagePerSecond, float Duration);
+	
+	
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
@@ -24,5 +49,35 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+
+	/*
+	Tracks the enemy's or player's current health status.
+	This value decreases when damage is taken and determines
+	when the character is defeated
+	*/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Health", meta = (AllowPrivateAccess = "true", ExposeOnSpawn = true))
+	float CurrentHealth = 0.f;
+
+
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Health", meta = (AllowPrivateAccess = "true", ExposeOnSpawn = true))
+	float MaxHealth = 100.f;
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Health", meta = (AllowPrivateAccess = "true", ExposeOnSpawn = true))
+	bool bMaxHealthSetOnStart;
+	
+	
+	//Event Dispatchers
+	UPROPERTY(BlueprintAssignable, Category = "Health Events")
+	FOnHealthChanged OnHealthChanged;
+
+	UPROPERTY(BlueprintAssignable, Category = "Health Events")
+	FOnDeath OnDeath;
+	
+private:
+	bool isDead = false;
+	FTimerDelegate timerDelegate;
+	FTimerHandle Timer;
+
 };

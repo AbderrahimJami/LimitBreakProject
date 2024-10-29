c9ab72cd2851d63309647f89a0e9b24dd4087704
Abderrahim Jami <abdrrahimjami@outlook.com>
2024-10-29 10:53:12 +0000

    Create new PlayerController and new PlayerController interface
    This new interface will be used by any class that wants to bind callback functions to PlayerController input events from the Enhanced Input SubSystem

---
+++ Source/LimitBreakSurvival/Private/CustomPlayerController.cpp
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CustomPlayerController.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+
+
+
+void ACustomPlayerController::OnMoveInputAction(const FInputActionValue& Value)
+{
+	FVector MoveInputVector = Value.Get<FVector>();
+	if (OnMoveInputEvent.IsBound())
+	{
+		OnMoveInputEvent.Broadcast(MoveInputVector);
+	}
+}
+
+void ACustomPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(this->GetLocalPlayer()))
+	{
+		Subsystem->AddMappingContext(PlayerMappingContext, 0);
+	}
+}
+
+void ACustomPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	//Bind Actions here!
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent))
+	{
+		EnhancedInputComponent->BindAction(MoveInputAction, ETriggerEvent::Triggered, this, &ACustomPlayerController::OnMoveInputAction);
+	}
+	
+}
+
+FMoveSignature* ACustomPlayerController::GetMoveDelegate()
+{
+	return &OnMoveInputEvent;
+}
---
+++ Source/LimitBreakSurvival/Public/CustomPlayerController.h
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InputActionValue.h"
+#include "PlayerControllerInterface.h"
+#include "GameFramework/PlayerController.h"
+#include "CustomPlayerController.generated.h"
+
+class UInputAction;
+class UInputMappingContext;
+/**
+ * 
+ */
+UCLASS()
+class LIMITBREAKSURVIVAL_API ACustomPlayerController : public APlayerController, public IPlayerControllerInterface
+{
+	GENERATED_BODY()
+
+	void OnMoveInputAction(const FInputActionValue& Value);
+public:
+	virtual void BeginPlay() override;
+	virtual  void SetupInputComponent() override;
+
+
+	//Delegates
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Input)
+	UInputMappingContext* PlayerMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Input)
+	UInputAction* MoveInputAction;
+
+	virtual FMoveSignature* GetMoveDelegate() override;
+
+private:
+	FMoveSignature OnMoveInputEvent;
+
+	
+};

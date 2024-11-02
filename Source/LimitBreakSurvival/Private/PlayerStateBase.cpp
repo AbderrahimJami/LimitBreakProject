fb6dd6d41109d3250e04fc4bc81068f420eae804
Abderrahim Jami <abdrrahimjami@outlook.com>
2024-11-02 12:42:49 +0000

    feat: add input binding, callback func and unbinding implementation

--- Source/LimitBreakSurvival/Private/PlayerStateBase.cpp
+++ Source/LimitBreakSurvival/Private/PlayerStateBase.cpp
@@ -12,6 +12,16 @@ void UPlayerStateBase::OnMoveAction(FVector MoveInput)
 	//Handle OnMoveAction Event triggered
 }
 
+void UPlayerStateBase::OnAimAction(FVector AimInput)
+{
+	//Handle OnAimAction Event triggered
+}
+
+void UPlayerStateBase::OnInteractAction()
+{
+	//Handle OnInputAction Event triggered
+}
+
 void UPlayerStateBase::EnterState_Implementation(ACharacter* player)
 {
 	if (!PlayerCharacterRef)
@@ -27,14 +37,21 @@ void UPlayerStateBase::EnterState_Implementation(ACharacter* player)
 		PlayerControllerRef = CastChecked<IPlayerControllerInterface>(UGameplayStatics::GetPlayerController(this, 0));
 		//Bind Functions
 	}
+
+	//Binding and Unbinding from Input delegates can be done in 2 ways here, either the base class binds to all input delegates,
+	//using a overridable virtual function actual states can then override and do something about, or you let them bind/unbind
+	//Individually.
 	PlayerControllerRef->GetMoveDelegate()->AddUObject(this, &UPlayerStateBase::OnMoveAction);
-	
+	PlayerControllerRef->GetAimDelegate()->AddUObject(this, &UPlayerStateBase::OnAimAction);
+	PlayerControllerRef->GetInteractDelegate()->AddUObject(this, &UPlayerStateBase::OnInteractAction);
 }	
 
 void UPlayerStateBase::ExitState_Implementation(ACharacter* Player)
 {
-	//Unbind all callback functions from OnMoveEvent Delegate
+	//Unbind all callback functions from Input event delegates
 	PlayerControllerRef->GetMoveDelegate()->RemoveAll(this);
+	PlayerControllerRef->GetAimDelegate()->RemoveAll(this);
+	PlayerControllerRef->GetInteractDelegate()->RemoveAll(this);
 }
 
 

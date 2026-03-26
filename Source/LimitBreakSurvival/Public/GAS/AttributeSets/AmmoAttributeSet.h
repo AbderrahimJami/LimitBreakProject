d461a07c32dc7876603c011a3c0e5d5a5cc43579
Abderrahim Jami <abdrrahimjami@outlook.com>
2026-03-26 17:17:21 +0000

    feat: add attribute set classes for Ammo, Health, Stamina, Weapon Handling, and Weapon Offensive

---
+++ Source/LimitBreakSurvival/Public/GAS/AttributeSets/AmmoAttributeSet.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "AmmoAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LIMITBREAKSURVIVAL_API UAmmoAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+};
